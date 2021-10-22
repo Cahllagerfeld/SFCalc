1cd9d404cc57d5716230de119173e5f7353260f9
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-10-22 19:54:46 +0200

    chore(settings): update config for gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -20,6 +21,9 @@ const config = {
 			assets: 'build',
 			fallback: null
 		}),
+		paths: {
+			base: dev ? '' : '/SFCalc',
+		},
 		appDir: 'internal',
 	}
 };
